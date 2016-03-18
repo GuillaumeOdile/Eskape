467e65aa45fefb919b6e1ae15546d95d7a2571bb
GuillaumeOdile <guillaume.odile12@gmail.com>
2016-03-18 16:26:35 +0100

    Added files via upload

---
+++ vald.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vald : MonoBehaviour
+{
+    bool arme = false;
+    bool is_walking = false;
+    bool dead = false;
+    Animator anim;
+  //  int jumpHash = Animator.StringToHash("idle");
+  //  int runStateHash = Animator.StringToHash("Base Layer.Walk");
+   
+    void estmort(bool test)
+    {
+        dead = test;
+    }
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        
+
+        if ((Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d")) && !arme && !dead)
+        {
+            anim.SetFloat("is_walking", 1F);
+            anim.SetFloat("arme", 0F);
+            anim.Play("walking");
+            is_walking = true;
+        }
+
+        if ((!Input.GetKey("d") && !Input.GetKey("a") && !Input.GetKey("s") && !Input.GetKey("w")) && !arme && !dead)
+        {
+            anim.SetFloat("is_walking", 0.0F);
+            anim.SetFloat("arme", 0F);
+            anim.Play("idle");
+        }
+
+        if (Input.GetKeyDown("e")) arme = !arme;
+        if (arme) anim.SetFloat("arme", 1F);
+
+        if ((Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d")) && arme && !dead)
+        {
+            anim.SetFloat("is_walking", 1F);
+            anim.SetFloat("arme", 1F);
+            anim.Play("walking_aiming");
+            is_walking = true;
+        }
+
+        if ((!Input.GetKey("d") && !Input.GetKey("a") && !Input.GetKey("s") && !Input.GetKey("w")) && arme && !dead)
+        {
+            anim.SetFloat("is_walking", 0.0F);
+            anim.SetFloat("arme", 1F);
+            anim.Play("idle_aiming");
+        }
+
+        Debug.Log("walk: " + is_walking);
+
+    }
+}
